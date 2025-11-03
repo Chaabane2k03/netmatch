4fac676060ae67ced4d289c52c1fd05448771844
Chaabane Boussadia <chaabaneboussadia@gmail.com>
2025-11-03 22:32:33 +0100

    adding splash screen (not native)

---
+++ lib/splash_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  _navigateToHome() async {
+    await Future.delayed(const Duration(seconds: 5)); // Duration of splash
+    if (mounted) {
+      Navigator.pushReplacementNamed(context, '/home');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white, // Set your preferred background color
+      body: Center(
+        child: Lottie.asset(
+          'assets/animations/cinema.json',
+          width: 300,
+          height: 300,
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
+  }
+}
